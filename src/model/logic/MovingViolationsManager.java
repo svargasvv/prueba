07fba60ffe6d71c469e83346334db7df823dba31
SANTIAGO VARGAS VEGA <s.vargasv@sis.virtual.uniandes.edu.co>
2019-02-04 20:29:00 -0500

    hola

--- src/model/logic/MovingViolationsManager.java
+++ src/model/logic/MovingViolationsManager.java
@@ -1,21 +1,48 @@
 package model.logic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 import api.IMovingViolationsManager;
 import model.vo.VOMovingViolations;
 import model.data_structures.LinkedList;
 
 public class MovingViolationsManager implements IMovingViolationsManager {
-
+	
+	private LinkedList<VOMovingViolations> arreglo;
 	
 	public void loadMovingViolations(String movingViolationsFile){
-		// TODO Auto-generated method stub
 		
+		File archivo = new File(movingViolationsFile);
+		try {
+			Scanner sca = new Scanner(archivo);
+			
+			while(sca.hasNext()){
+				sca.nextLine();
+				String actual = sca.next();
+				StringTokenizer st = new StringTokenizer(actual,",");
+				
+				while(st.hasMoreTokens()){
+					ArrayList<String> datosActual = new ArrayList<String>();
+					datosActual.add(st.nextToken());
+					arreglo.add(new VOMovingViolations(datosActual.get(0), datosActual.get(1), datosActual.get(2), datosActual.get(3), datosActual.get(4), datosActual.get(5)));
+
+				}
+	
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 		
 	@Override
 	public LinkedList <VOMovingViolations> getMovingViolationsByViolationCode (String violationCode) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
--- src/model/vo/VOMovingViolations.java
+++ src/model/vo/VOMovingViolations.java
@@ -7,6 +7,12 @@ public class VOMovingViolations {
 	
 	
 	
+	public VOMovingViolations(String string, String string2, String string3, String string4, String string5,
+			String string6) {
+		// TODO Auto-generated constructor stub
+	}
+
+
 	/**
 	 * @return id - Identificador único de la infracción
 	 */
